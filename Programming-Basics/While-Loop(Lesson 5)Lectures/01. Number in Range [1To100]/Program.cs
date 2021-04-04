c67f06992788f594dd31372a3bb4c21f5bbab7ef
ivelco <ivelin_conchev@abv.bg>
2021-04-04 09:37:26 +0300

    Add Programming Lesson 5 Lab

---
+++ Programming-Basics/While-Loop(Lesson 5)Lectures/01. Number in Range [1To100]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Number_in_Range__1To100_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            while (num < 1 || num > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The number is: " + num);
+        }
+    }
+}
---
+++ Programming-Basics/While-Loop(Lesson 5)Lectures/05. Max Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int currentMax = int.MinValue;
+            while (n > 0 )
+            {
+                int nextNum = int.Parse(Console.ReadLine());
+                if (nextNum > currentMax)
+                {
+                    currentMax = nextNum;
+                }
+                n--;
+            }
+            Console.WriteLine(currentMax );
+        }
+    }
+}
