c75ff2e4860ca326f192267c920b0f88a1bab638
ivelco <ivelin_conchev@abv.bg>
2021-04-14 18:29:09 +0300

    Add Programming Fundamentals Lesson 1 Excercises

---
+++ Programming-Fundamentals/Exercise Intro and Basic Syntax/6. Strong Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _6._Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int mod = 1;
+            int sum = 0;
+            int number = numberOne;
+            while (number > 0)
+            {
+                mod = number % 10;
+                number /= 10;
+
+                int fact = 1;
+                for (int i = 1; i <= mod; i++)
+                {
+                    fact = fact * i;
+                }
+                sum += fact;
+                if (numberOne == sum)
+                {
+                    break;
+                }
+            }
+            if (numberOne == sum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
