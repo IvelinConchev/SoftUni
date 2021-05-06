4e0f1c82c10cd5c67d4b1f49925f9ed4b098ecf0
ivelco <ivelin_conchev@abv.bg>
2021-05-06 20:12:23 +0300

    Add Programming Fundamentals Lesson 8

---
+++ Programming-Fundamentals/Text processing-Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string reverse = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                    reverse = string.Empty;
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reverse += input[i];
+                }
+                Console.WriteLine($"{input} = {reverse}");
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals/Text processing-Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultDigit = string.Empty;
+            string resultLetter = string.Empty;
+            string resultOther = string.Empty;
+
+            string text = Console.ReadLine();
+
+            foreach (var symbol in text)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    resultDigit += symbol;
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    resultLetter += symbol;
+                }
+                else
+                {
+                    resultOther += symbol;
+                }
+            }
+            Console.WriteLine(resultDigit);
+            Console.WriteLine(resultLetter);
+            Console.WriteLine(resultOther);
+        }
+    }
+}
